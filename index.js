8f585929aafa0b5b34a8be9e5f6fe753d5d0ea39
ThawThar <thawtharshin@gmail.com>
2023-10-25 10:35:22 +0630

    first

---
+++ index.js
@@ -0,0 +1,82 @@
+// for(var i=0;i<5;i++)
+// {
+//     console.log("Aung"+i);
+// }
+
+// for(var k=9;k>=5;k--)
+// {
+//     console.log(k);
+// }
+
+
+// for(var j=0;j<10;j++)
+// {
+//     for(var i=0;i<5;i++)
+//     {
+//         for(var k=10;k<15;k++)
+//         {
+//             console.log(k+"*"+i+"#"+j);
+//         }
+//     }
+// }
+
+
+
+// for(var i=0;i<10;i++)
+// {
+//     if(i%2==1){
+//         console.log(i);
+//     }
+// }
+
+
+// for(var j=0;j<5;j++)
+// {
+//        for( var l=0;l<j;l++){
+//             document.write("*");
+//        }
+//     //    if(j>0){
+//         document.write("<br>");
+//     //    }
+       
+// }
+
+
+
+// for(var k=1;k<6;k++)
+// {
+//     for(var l=0;l<k;l++)
+//     {
+//         document.write("*");
+//     }
+//     document.write("<br>");
+    
+//     if(k==5){
+//         for(var i=k+1;i>0;i--)
+//         {
+//             for(var j=0;j<i;j++)
+//             {
+//                 document.write("*");
+//             }
+//              document.write("<br>");
+//         }
+//     }
+// }
+
+
+
+
+
+for(var c=0;c<3;c++)
+{
+    for(var d=0;d<c;d++)
+    {
+        document.write("*"+"<br>");
+    }
+    if(c==1)
+    for(var e=c;e<3;e++)
+        {
+            document.write("*");
+        }
+}
+
